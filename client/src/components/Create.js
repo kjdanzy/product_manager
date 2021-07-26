aba7a430b4ffbd35b485841e66881bc9cf3c2db6
kjdanzy <64291380+kjdanzy@users.noreply.github.com>
2021-07-25 22:45:48 -0500

    added validation

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -9,6 +9,8 @@ const Create = () => {
         description: ""
     })
 
+    const [formErrors, setFormErrors] = useState({})
+
     const onSubmitHandler = (event) => {
         event.preventDefault();
         axios.post("http://localhost:8000/api/product/new", formInfo)
@@ -17,10 +19,16 @@ const Create = () => {
                 console.log("insert successful!")
                 console.log(res)
                 console.log("*************************************************")
+                if (res.data.results) {
+                    navigate("/")
+                }
+                else {
+                    console.log("Whoops!!  You have to fill this form out properly!")
+                    setFormErrors(res.data.error.errors)
+                }
             })
             .catch((err) => console.log("?????  This error was produced during the save operation.   ??????", err))
         console.log("http://localhost:8000/api/product/new")
-        navigate("/")
     }
 
     const changeHandler = (event) => {
@@ -37,12 +45,13 @@ const Create = () => {
                     <h4>Use this form to add a new product!</h4>
                     <label>Title:  </label>
                     <input type="text" name="title" onChange={changeHandler} />
+                    {formErrors.title ? <p className="text-danger">{formErrors.title.message} </p>: ""}
                 </div>
                 <div>
                     <br />
                     <label>Price:  </label>
                     <input type="text" name="price" onChange={changeHandler} />
-
+                    {formErrors.price ? <p className="text-danger">{formErrors.price.message} </p>: ""}
                 </div>
                 <div>
                     <br />
@@ -54,6 +63,7 @@ const Create = () => {
                         rows={5}
                         placeholder="Please enter a complete description" />                </div>
                 <input type="submit" value="Submit" />
+                {formErrors.description ? <p className="text-danger">{formErrors.description.message} </p>: ""}
             </form>
             <br />
         </div>
--- server/models/product.model.js
+++ server/models/product.model.js
@@ -4,15 +4,20 @@ const mongoose = require("mongoose")
 const ProductSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: [true, "The title is required"]
+        required: [true, "The title is required"],
+        minLength: [3, "The title must contain more than 3 characters."]
     },
     price: {
         type: Number,
-        required: [true, "The price is required"]
+        required: [true, "The price is required"],
+        min: [0, "The price must be more than 0; no freebies here!"],
+        max: [1000000000, "So, you'd like to by the company?! Otherwise, we don't have anything that expensive here!"],
     },
     description: {
         type: String,
-        required: [true, "The description is required"]
+        required: [true, "The description is required"],
+        minLength: [3, "The description must contain more than 3 characters. I 'heart' you doesn't count!"],
+        maxLength: [300, "Woah there buddy!!  Are you writing a dissertation??!"]
     },
 })
 
